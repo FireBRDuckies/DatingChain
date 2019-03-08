ad2f29f0b27ffac45c1a85a26cb86f717f78de5d
noble-gas <bansalakshit56@gmail.com>
2019-03-09 04:25:08 +0530

    added sign in

--- app/src/main/java/com/fbrd/datingchain/MainActivity.kt
+++ app/src/main/java/com/fbrd/datingchain/MainActivity.kt
@@ -2,12 +2,52 @@ package com.fbrd.datingchain
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var mCurrentUser: FirebaseUser? = null
+    private lateinit var mFirebaseAuth: FirebaseAuth
+    private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mAuthStateListener = FirebaseAuth.AuthStateListener {
+            mCurrentUser = mFirebaseAuth.currentUser
+
+            if (mCurrentUser == null) {
+                // User not logged in
+
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                            arrayListOf(
+                                AuthUI.IdpConfig.GoogleBuilder().build()
+                            )
+                        )
+                        .build(),
+                    RC_LOGIN
+                )
+
+            } else {
+                // User logged in
+
+                text.text = "${mCurrentUser?.displayName}"
+            }
+        }
+
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener)
+    }
+
+    companion object {
+        const val RC_LOGIN = 5
     }
 }
---
+++ app/src/main/java/com/fbrd/datingchain/ProfileInputActivity.kt
@@ -0,0 +1,12 @@
+package com.fbrd.datingchain
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileInputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_input)
+    }
+}
